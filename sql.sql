7994397e7d49efc8534c344c109252241288ea2f
Oliver <oliverskytte@gmail.com>
2021-03-29 21:29:01 +0200

    1

---
+++ sql.sql
@@ -0,0 +1,31 @@
+
+CREATE VIEW Finansposter AS
+SELECT 
+    CAST([G_L Account No_] AS VARCHAR)                                  AS 'Kontonummer',--STD
+    [Posting Date]                                                      AS 'Bogføringsdato',--STD
+    '***regnskab***'                                                    AS 'Regnskab',--STD
+    [Amount]                                                            AS 'Beløb',--STD
+    CONCAT('***regnskab***', '_', 
+    CAST([G_L Account No_] AS VARCHAR))                                 AS 'Key_Company_Account_No',
+    CONCAT('***regnskab***','_', [Dimension Set ID])                AS 'Key_Company_Dimension Set ID', 
+	CONCAT([Source Type],'_', [Source No_])							AS 'Key_Source_Type_Code'
+FROM ***schema***.dbo.[***regnskab***$G_L Entry]
+GO
+
+CREATE VIEW Kunde AS
+SELECT 
+    '1_' + [No_]                                                        AS 'Key_Source_Type_Code',--STD
+    [No_]                                                               AS 'Kundenummer',--STD
+    C.Name                                                              AS 'Kundenavn',--STD
+    [Salesperson Code]                                                  AS 'Salgsperson',--STD
+    C.[Country_Region Code]                                             AS 'Landekode',
+    CR.[Name]                                                           AS 'Land',
+    [Post Code]                                                         AS 'Postnummer',
+    PC.City                                                             AS 'By',
+    '***regnskab***'                                                    AS 'Regnskab'
+FROM ***schema***.dbo.[***regnskab***$Customer] AS C
+LEFT JOIN ***schema***.dbo.[***regnskab***$Country_Region] AS CR
+    ON C.[Country_Region Code] = CR.Code
+LEFT JOIN ***schema***.dbo.[***regnskab***$Post Code] AS PC
+    ON C.[Post Code] = PC.Code
+GO
